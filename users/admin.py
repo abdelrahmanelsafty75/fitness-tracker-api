73389e4429cb059b61e7842d40a39ca61cb733da
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-03-07 00:07:18 +0200

    feat: customize admin panel for users with fieldsets to be organized

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_active', 'created_at']
+    
+    list_filter = ['is_active', 'is_staff', 'created_at']
+    
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
