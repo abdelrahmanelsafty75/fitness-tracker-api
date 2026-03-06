b3e7ddf2fce140c5b56db29fbbb8ebabc35a3ca8
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-03-07 00:07:45 +0200

    feat: customize admin panel for activities with fieldsets to be organized

--- activities/admin.py
+++ activities/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Activity
 
-# Register your models here.
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ['user', 'activity_type', 'duration', 'distance', 'calories_burned', 'date', 'created_at']
+    list_filter = ['activity_type', 'date', 'created_at']
+    search_fields = ['user__username', 'notes', 'activity_type']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    date_hierarchy = 'date'
+    
+    fieldsets = (
+        ('Activity Details', {
+            'fields': ('user', 'activity_type', 'date')
+        }),
+        ('Metrics', {
+            'fields': ('duration', 'distance', 'calories_burned')
+        }),
+        ('Additional Info', {
+            'fields': ('notes', 'created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
